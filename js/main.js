bbccf35a8a2b8d955b82c050c616a37c2546617d
Aidar <aidar.lutfullin.1992@gmail.com>
2021-08-25 08:57:02 +0300

    commit 5

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 $(function () {
+  $(".menu__btn").on("click", function () {
+    $(".menu__list").toggleClass("menu__list--active");
+  });
+
   $(".heroes__slider-img").slick({
     slidesToShow: 1,
     slidesToScroll: 1,
